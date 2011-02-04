8a123a511b731cf9897d11632e724470d9fb795d
kmiddleton <kmiddleton@fa99769c-8e5b-46ab-872a-74f3620d8049>
2011-02-04 01:33:34 +0000

    Add welchANOVA() Fix .Rd files to include format. Add plyr as dependency.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/zar5@13 fa99769c-8e5b-46ab-872a-74f3620d8049

--- pkg/R/Zar5-package.R
+++ pkg/R/Zar5-package.R
@@ -1,310 +1,335 @@
-#' Zar5
-#' 
-#' Data sets and example code for Zar's Biostatistical Analysis (5th Edition).
-#' 
-#' \tabular{ll}{ 
-#'	Package: \tab Zar5\cr
-#'	Type: \tab Package\cr
-#'	Version: \tab 0.1-3\cr
-#'	Date: \tab 2011-01-30\cr
-#'	License: \tab GPL\cr
-#'	LazyLoad: \tab yes\cr
-#'      LazyData: \tab yes\cr
-#'      }
-#' 
-#' @name Zar5-package
-#' 
-#' @aliases Zar5-package Zar5
-#' 
-#' @docType package
-#' 
-#' @author Kevin Middleton (\email{kmm@@csusb.edu})
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords package
+##' Zar5
+##' 
+##' Data sets and example code for Zar's Biostatistical Analysis (5th Edition).
+##' 
+##' \tabular{ll}{
+##' Package: \tab Zar5\cr
+##' Type: \tab Package\cr
+##' Version: \tab 0.1-3\cr
+##' Date: \tab 2011-01-30\cr
+##' License: \tab GPL\cr
+##' LazyLoad: \tab yes\cr
+##' LazyData: \tab yes\cr
+##' }
+##' 
+##' @name Zar5-package
+##' 
+##' @aliases Zar5-package Zar5
+##' 
+##' @docType package
+##' 
+##' @author Kevin Middleton (\email{kmm@@csusb.edu})
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##' Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##' ISBN-13: 978013100846.
+##' \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords package
 NULL
 
 
 
 
 
-#' Zar Data Set: ex08.08
-#'
-#' Zar Data Set: ex08.08
-#' 
-#' 
-#' @name ex08.08
-#' 
-#' @docType data
-#' 
-#' @format The format is: List of 2
-#'         \describe{ \item{$Weight}{: num [1:10] 72.5 71.7 60.8 63.2 71.4 73.1 77.9 75.7 72 69}
-#'         \item{$Height}{: num [1:11] 183 172 180 190 191 ...}
-#'  }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex08.08)
-#' 
+##' Zar Data Set: ex08.08
+##'
+##' Zar Data Set: ex08.08
+##' 
+##' @title Zar Data Set: ex08.08
+##' 
+##' @name ex08.08
+##' 
+##' @docType data
+##' 
+##' @format The format is: List of 2
+##' \describe{
+##' \item{\code{$Weight}}{num [1:10] 72.5 71.7 60.8 63.2 71.4 73.1 77.9 75.7 72 69}
+##' \item{\code{$Height}}{num [1:11] 183 172 180 190 191 ...}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##'   ISBN-13: 978013100846.
+##'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex08.08)
+##' 
 NULL
 
 
 
 
 
-#' Zar Data Set: ex08.11
-#'
-#' Zar Data Set: ex08.11
-#' 
-#' 
-#' @name ex08.11
-#' 
-#' @docType data
-#' 
-#' @format The format is: List of 2 \describe{ \item{$Males}{: num [1:7] 193
-#'   188 185 183 180 175 170} \item{$Females}{: num [1:5] 178 173 168 165 163}
-#'   }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex08.11)
-#' 
+##' Zar Data Set: ex08.11
+##'
+##' Zar Data Set: ex08.11
+##' 
+##' @title Zar Data Set: ex08.11
+##' 
+##' @name ex08.11
+##' 
+##' @docType data
+##' 
+##' @format The format is: List of 2
+##' \describe{
+##' \item{\code{$Males}}{num [1:7] 193 188 185 183 180 175 170}
+##' \item{\code{$Females}}{num [1:5] 178 173 168 165 163}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##' Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##' ISBN-13: 978013100846.
+##' \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex08.11)
+##' 
 NULL
 
 
 
 
 
-#' Zar Data Set: ex10.01
-#'
-#' Zar Data Set: ex10.01
-#' 
-#' 
-#' @name ex10.01
-#' 
-#' @docType data
-#' 
-#' @format A data frame with 20 observations on the following 2 variables.
-#'   \describe{ \item{list("X")}{a numeric vector} \item{list("Feed")}{a factor
-#'   with levels \code{1}, \code{2}, \code{3}, and \code{4}} }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex10.01)
-#' 
+##' Zar Data Set: ex10.01
+##'
+##' Zar Data Set: ex10.01
+##' 
+##' @title Zar Data Set: ex10.01
+##' 
+##' @name ex10.01
+##' 
+##' @docType data
+##' 
+##' @format A data frame with 20 observations on the following 2 variables.
+##' \describe{
+##' \item{\code{X}}{a numeric vector}
+##' \item{\code{Feed}}{a factor with levels \code{1}, \code{2}, \code{3}, and \code{4}}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##'   ISBN-13: 978013100846.
+##'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex10.01)
+##' 
 NULL
 
 
 
 
 
-#' Zar Data Set: ex10.02
-#'
-#' Zar Data Set: ex10.02
-#' 
-#' 
-#' @name ex10.02
-#' 
-#' @docType data
-#' 
-#' @format A data frame with 20 observations on the following 2 variables.
-#'   \describe{ \item{list("X")}{a numeric vector} \item{list("Tech")}{a factor
-#'   with levels \code{1}, \code{2}, \code{3}, and \code{4}} }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex10.02)
-#' 
+##' Zar Data Set: ex10.02
+##'
+##' Zar Data Set: ex10.02
+##' 
+##' @title Zar Data Set: ex10.02
+##' 
+##' @name ex10.02
+##' 
+##' @docType data
+##' 
+##' @format A data frame with 20 observations on the following 2 variables.
+##' \describe{
+##' \item{\code{X}}{a numeric vector}
+##' \item{\code{Tech}}{a factor with levels \code{1}, \code{2}, \code{3}, and \code{4}}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##'   ISBN-13: 978013100846.
+##'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex10.02)
+##' 
 NULL
 
 
 
 
 
-#' Zar Data Set: ex12.01
-#'
-#' Zar Data Set: ex12.01
-#' 
-#' 
-#' @name ex12.01
-#' 
-#' @docType data
-#' 
-#' @format A data frame with 20 observations on the following 3 variables.
-#'   \describe{ \item{list("Ca")}{a numeric vector} \item{list("Trt")}{a factor
-#'   with levels \code{No} and \code{Yes}} \item{list("Sex")}{a factor with
-#'   levels \code{F} and \code{M}} }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex12.01)
-#' 
+##' Zar Data Set: ex12.01
+##'
+##' Zar Data Set: ex12.01
+##' 
+##' @title Zar Data Set: ex12.01
+##' 
+##' @name ex12.01
+##' 
+##' @docType data
+##' 
+##' @format A data frame with 20 observations on the following 3 variables.
+##' \describe{
+##' \item{\code{Ca}}{a numeric vector}
+##' \item{\code{Trt}}{a factor with levels \code{No} and \code{Yes}}
+##' \item{\code{Sex}}{a factor with levels \code{F} and \code{M}}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##'   ISBN-13: 978013100846.
+##'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex12.01)
+##' 
 NULL
 
 
 
 
 
-#' Zar Data Set: ex13.03
-#'
-#' Zar Data Set: ex13.03
-#' 
-#' 
-#' @name ex13.03
-#' 
-#' @docType data
-#' 
-#' @format A data frame with 5 observations on the following 4 variables.
-#'   \describe{
-#'             \item{list("Group.1")}{a numeric vector}
-#'             \item{list("Group.2")}{a numeric vector}
-#'             \item{list("Group.3")}{a numeric vector}
-#'             \item{list("Group.4")}{a numeric vector}
-#'  }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex13.03)
-#' 
+##' Zar Data Set: ex13.03
+##'
+##' Zar Data Set: ex13.03
+##' 
+##' @title Zar Data Set: ex13.03
+##' 
+##' @name ex13.03
+##' 
+##' @docType data
+##' 
+##' @format A data frame with 5 observations on the following 4 variables.
+##' \describe{
+##' \item{\code{Group.1}}{a numeric vector}
+##' \item{\code{Group.2}}{a numeric vector}
+##' \item{\code{Group.3}}{a numeric vector}
+##' \item{\code{Group.4}}{a numeric vector}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##'   ISBN-13: 978013100846.
+##'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex13.03)
+##' 
 NULL
 
 
 
 
 
-#' Zar Data Set: ex13.04
-#'
-#' Zar Data Set: ex13.04
-#' 
-#' 
-#' @name ex13.04
-#' 
-#' @docType data
-#' 
-#' @format A data frame with 7 observations on the following 2 variables.
-#'   \describe{ \item{list("insecticide1")}{a numeric vector}
-#'   \item{list("insecticide2")}{a numeric vector} }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex13.04)
-#' 
+##' Zar Data Set: ex13.04
+##'
+##' Zar Data Set: ex13.04
+##' 
+##' @title Zar Data Set: ex13.04
+##' 
+##' @name ex13.04
+##' 
+##' @docType data
+##' 
+##' @format A data frame with 7 observations on the following 2 variables.
+##' \describe{
+##' \item{\code{insecticide1}}{a numeric vector}
+##' \item{\code{insecticide2}}{a numeric vector}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##'   ISBN-13: 978013100846.
+##'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex13.04)
+##' 
 NULL
 
 
 
 
 
-#' Zar Data Set: ex15.01
-#'
-#' Zar Data Set: ex15.01
-#' 
-#' 
-#' @name ex15.01
-#' 
-#' @docType data
-#' 
-#' @format A data frame with 12 observations on the following 3 variables.
-#'   \describe{ \item{list("Chol")}{a numeric vector} \item{list("Drug")}{a
-#'   factor with levels \code{1}, \code{2}, and \code{3}}
-#'   \item{list("Source")}{a factor with levels \code{A}, \code{B}, \code{D},
-#'   \code{L}, \code{Q}, and \code{S}} }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex15.01)
-#' 
+##' Zar Data Set: ex15.01
+##'
+##' Zar Data Set: ex15.01
+##' 
+##' @title Zar Data Set: ex15.01
+##' 
+##' @name ex15.01
+##' 
+##' @docType data
+##' 
+##' @format A data frame with 12 observations on the following 3 variables.
+##' \describe{
+##' \item{\code{Chol}}{a numeric vector}
+##' \item{\code{Drug}}{a factor with levels \code{1}, \code{2}, and \code{3}}
+##' \item{\code{Source}}{a factor with levels \code{A}, \code{B}, \code{D},
+##'   \code{L}, \code{Q}, and \code{S}}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##'   ISBN-13: 978013100846.
+##'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex15.01)
+##' 
 NULL
 
 
 
 
 
-#' Zar Data Set: ex16.01
-#'
-#' Zar Data Set: ex16.01
-#' 
-#' 
-#' @name ex16.01
-#' 
-#' @docType data
-#' 
-#' @format A data frame with 20 observations on the following 3 variables.
-#'   \describe{ \item{list("fat")}{a numeric vector} \item{list("lean")}{a
-#'   numeric vector} \item{list("month")}{a factor with levels \code{Dec},
-#'   \code{Feb}, \code{Jan}, and \code{Mar}} }
-#' 
-#' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
-#'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
-#'   ISBN-13: 978013100846.
-#'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
-#' 
-#' @keywords datasets
-#' 
-#' @examples
-#' 
-#' demo(ex16.01)
-#' 
+##' Zar Data Set: ex16.01
+##'
+##' Zar Data Set: ex16.01
+##' 
+##' @title Zar Data Set: ex16.01
+##' 
+##' @name ex16.01
+##' 
+##' @docType data
+##' 
+##' @format A data frame with 20 observations on the following 3 variables.
+##' \describe{
+##' \item{\code{fat}}{a numeric vector}
+##' \item{\code{lean}}{a numeric vector}
+##' \item{\code{month}}{a factor with levels \code{Dec},
+##'   \code{Feb}, \code{Jan}, and \code{Mar}}
+##' }
+##' 
+##' @references Zar, J.H. 2010. \emph{Biostatistical Analysis (5th Edition)}.
+##'   Pearson Prentice-Hall. Upper Saddle River, NJ. ISBN-10: 0131008463.
+##'   ISBN-13: 978013100846.
+##'   \url{http://www.pearsonhighered.com/educator/product/Biostatistical-Analysis/9780131008465.page}
+##' 
+##' @keywords datasets
+##' 
+##' @examples
+##' 
+##' demo(ex16.01)
+##' 
 NULL
 
 
