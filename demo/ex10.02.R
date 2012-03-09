f1230876be15ce0ef323a48268e5838cc0bd09e6
Kevin Middleton <kmm@csusb.edu>
2012-03-09 09:56:30 -0800

    Add closing ) in demo/ex10.02

--- demo/ex10.02.R
+++ demo/ex10.02.R
@@ -3,7 +3,7 @@ str(ex10.02)
 ddply(ex10.02, .(Tech),
       function(x)c(n = length(x$X),
                    Mean = mean(x$X),
-                   Variance = var(x$X))
+                   Variance = var(x$X)))
 
 fm1 <- aov(X ~ Tech, data = ex10.02)
 summary(fm1)
