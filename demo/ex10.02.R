353f681c97d9ff1ee710da01720e35f9ca977b25
Kevin Middleton <kmm@csusb.edu>
2012-03-09 09:41:41 -0800

    Update demo/ex10.02

--- demo/ex10.02.R
+++ demo/ex10.02.R
@@ -1,7 +1,12 @@
 str(ex10.02)
 
-mod1 <- aov(X ~ Tech, data = ex10.02)
-summary(mod1)
+ddply(ex10.02, .(Tech),
+      function(x)c(n = length(x$X),
+                   Mean = mean(x$X),
+                   Variance = var(x$X))
 
-mod2 <- aov(X ~ 1 + Error(Tech), data = ex10.02)
-summary(mod2)
+fm1 <- aov(X ~ Tech, data = ex10.02)
+summary(fm1)
+
+fm2 <- aov(X ~ 1 + Error(Tech), data = ex10.02)
+summary(fm2)
