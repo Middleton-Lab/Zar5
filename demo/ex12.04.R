8c39cbf9d30a927713e6e65f23d7e5feff6deea6
Kevin Middleton <middletonk@missouri.edu>
2016-02-17 14:24:45 -0600

    Add example 12.04

---
+++ demo/ex12.04.R
@@ -0,0 +1,7 @@
+str(ex12.04)
+
+ex12.04 <- ex12.04 %>% mutate(Block = factor(Block),
+                              Treatment = factor(Treatment))
+
+fm <- lm(Time ~ Treatment + Block, data = ex12.04)
+anova(fm)
